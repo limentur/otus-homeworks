e9fdcee8af0fa7395054c525c02b1ba1c6d199ab
Simon <limentur@yandex.ru>
2024-12-07 14:26:17 +0300

    hw03 ready to check after resolving fixes

--- L06-annotations/src/main/java/ru/otus/TestStarter.java
+++ L06-annotations/src/main/java/ru/otus/TestStarter.java
@@ -1,5 +1,7 @@
 package ru.otus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.annotations.After;
 import ru.otus.annotations.Before;
 import ru.otus.annotations.Test;
@@ -7,49 +9,65 @@ import ru.otus.annotations.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class TestStarter extends TestStarterForFieldsExtention {
+public class TestStarter {
+    private static final Logger logger = LoggerFactory.getLogger(TestStarter.class);
+
 
     public TestStarter(TestSet set) {
         parseAndRun(set);
-        showTestStats();
     }
 
     private void parseAndRun(Object object){
-        findBeforeAndAfterAnnotations(object.getClass());
-
+        Method beforeMethod = findBeforeAnnotations(object.getClass());
+        Method afterMethod = findAfterAnnotations(object.getClass());
+        int testPassed = 0;
+        int testFailed = 0;
         for (var method : object.getClass().getDeclaredMethods()){
             if (method.isAnnotationPresent(Test.class)){
-                invokeMethodAnnotations(method,object);
+                if((invokeMethodAnnotations(method,object,beforeMethod, afterMethod) == 1)){
+                    testPassed+=1;
+                }
+                else {
+                    testFailed +=1;
+                }
             }
         }
+        showTestStats(testPassed,testFailed);
     }
 
-    private void findBeforeAndAfterAnnotations(Class<?> t){
+    private Method findBeforeAnnotations(Class<?> t){
         for (var method : t.getDeclaredMethods()) {
             if (method.isAnnotationPresent(Before.class)){
-                beforeMethod = method;
-            }
-            if (method.isAnnotationPresent(After.class)){
-                afterMethod = method;
+                return method;
             }
         }
+        return null;
     }
 
-    private void invokeMethodAnnotations(Method method, Object object) {
+    private Method findAfterAnnotations(Class<?> t){
+        for (var method : t.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(After.class)){
+                 return method;
+            }
+        }
+        return null;
+    }
+
+    private int invokeMethodAnnotations(Method method, Object object,Method beforeMethod, Method afterMethod) {
         try {
             logger.info("Invoking {}", method.getName());
             beforeMethod.invoke(object);
             method.invoke(object);
-            testPassed+=1;
             afterMethod.invoke(object);
             logger.info("-------------");
+            return 1;
         } catch (IllegalAccessException | InvocationTargetException e) {
             logger.info("Exception {}, in {}", e.getCause(), method.getName());
-            testFailed+=1;
+            return 0;
         }
     }
 
-    private void showTestStats(){
+    private void showTestStats(int testPassed,int testFailed){
         logger.info("Tests passed {}", testPassed);
         logger.info("Tests failed {}", testFailed);
     }
