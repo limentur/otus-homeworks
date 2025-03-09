f2a49c1738a38ca9cc8d53843970c53df488bcf1
Simon <limentur@yandex.ru>
2025-03-09 09:59:59 +0300

    hw07 ready to check

---
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/HomeWork.java
@@ -0,0 +1,52 @@
+package ru.otus;
+
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.homework.HistoryListener;
+import ru.otus.model.Message;
+import ru.otus.processor.LoggerProcessor;
+import ru.otus.processor.homework.ProcessorFieldSwitch;
+import ru.otus.processor.homework.ProcessorThrowExceptionEverySec;
+
+import java.util.List;
+
+public class HomeWork {
+
+    /*
+    Реализовать to do:
+      1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
+      2. Сделать процессор, который поменяет местами значения field11 и field12
+      3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
+            Секунда должна определяьться во время выполнения.
+            Тест - важная часть задания
+            Обязательно посмотрите пример к паттерну Мементо!
+      4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
+         Уже есть заготовка - класс HistoryListener, надо сделать его реализацию
+         Для него уже есть тест, убедитесь, что тест проходит
+    */
+
+    public static void main(String[] args) {
+        var processors = List.of(new ProcessorFieldSwitch(), new LoggerProcessor(new ProcessorThrowExceptionEverySec()));
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {});
+        var listenerPrinter = new HistoryListener();
+        complexProcessor.addListener(listenerPrinter);
+
+        var message = new Message.Builder(1L)
+                .field1("field1")
+                .field2("field2")
+                .field3("field3")
+                .field6("field6")
+                .field10("field10")
+                .field11("field11")
+                .field12("field12")
+                .build();
+
+        complexProcessor.handle(message);
+
+        complexProcessor.removeListener(listenerPrinter);
+        /*
+          по аналогии с Demo.class
+          из элеменов "to do" создать new ComplexProcessor и обработать сообщение
+        */
+    }
+}
---
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/processor/homework/EvenSecondException.java
@@ -0,0 +1,8 @@
+package ru.otus.processor.homework;
+
+public class EvenSecondException extends Exception{
+
+    public EvenSecondException(String message) {
+        super(message);
+    }
+}
